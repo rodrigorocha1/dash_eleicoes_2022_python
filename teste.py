e52be8b6558e4efdebecea38766aa902fc23463b
silva <silva.rodrigo31@outlook.com>
2023-02-11 15:16:16 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,14 @@
+import dash
+from dash import html
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div(
+    children=[
+        html.H1('Teste Nisso aquiaaassss')
+    ]
+)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
